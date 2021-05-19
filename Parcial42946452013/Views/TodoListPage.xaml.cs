870f79d50928dc97b39208058b71853dc6ce3dea
Perdomo24 <rperdomo32@gmail.com>
2021-05-19 11:30:16 -0600

    Parcial 4 Xamarin

---
+++ Parcial42946452013/Views/TodoListPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Parcial42946452013.Models;
+using System;
+using Xamarin.Forms;
+
+namespace Parcial42946452013
+{
+    public partial class TodoListPage : ContentPage
+    {
+        public TodoListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            listView.ItemsSource = await App.Database.GetItemsAsync();
+        }
+
+        async void OnItemAdded(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TodoItemPage
+            {
+                BindingContext = new TodoItem()
+            });
+        }
+
+        async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new TodoItemPage
+                {
+                    BindingContext = e.SelectedItem as TodoItem
+                });
+            }
+        }
+    }
+}
